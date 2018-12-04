6038eb64dab7d1d6ae0e96f7f5f0a27b61b167ab
Dan Nixon <dan@dan-nixon.com>
2018-12-04 13:20:52 +0000

    Tidy release build script

--- mantid/build_release.sh
+++ mantid/build_release.sh
@@ -1,46 +1,32 @@
 #!/bin/bash
 
-IMAGE="mantidproject/mantid"
 VERSION=$1
+IMAGE="mantidproject/mantid"
+BUILD_LOG_DIR="build_logs"
 
-# Version only tag (default to CentOS 7)
-docker build \
-  -f Dockerfile_CentOS7_Release \
-  -t ${IMAGE}:${VERSION} \
-  --build-arg MANTID_VERSION=${VERSION} \
-  .
+function do_build {
+  DOCKERFILE=$1
+  TAG=$2
+
+  mkdir -p ${BUILD_LOG_DIR}
+  docker build --file=${DOCKERFILE} --tag=${IMAGE}:${TAG} --build-arg MANTID_VERSION=${VERSION} . | tee "${BUILD_LOG_DIR}/${TAG}.log"
+}
 
 # "latest" tag (default to CentOS 7)
-docker build \
-  -f Dockerfile_CentOS7_Release \
-  -t ${IMAGE}:latest \
-  --build-arg MANTID_VERSION=${VERSION} \
-  .
-
-# Versioned CentOS 7 tag
-docker build \
-  -f Dockerfile_CentOS7_Release \
-  -t ${IMAGE}:${VERSION}_centos7 \
-  --build-arg MANTID_VERSION=${VERSION} \
-  .
+do_build Dockerfile_CentOS7_Release latest
 
 # "latest" CentOS 7 tag
-docker build \
-  -f Dockerfile_CentOS7_Release \
-  -t ${IMAGE}:latest_centos7 \
-  --build-arg MANTID_VERSION=${VERSION} \
-  .
-
-# Versioned Ubuntu 16.04 (xenial) tag
-docker build \
-  -f Dockerfile_Ubuntu16.04_Release \
-  -t ${IMAGE}:${VERSION}_ubuntu16.04 \
-  --build-arg MANTID_VERSION=${VERSION} \
-  .
+do_build Dockerfile_CentOS7_Release latest_centos7
 
 # "latest" Ubuntu 16.04 (xenial) tag
-docker build \
-  -f Dockerfile_Ubuntu16.04_Release \
-  -t ${IMAGE}:latest_ubuntu16.04 \
-  --build-arg MANTID_VERSION=${VERSION} \
-  .
+do_build Dockerfile_Ubuntu16.04_Release latest_ubuntu16.04
+
+# Version only tag (default to CentOS 7)
+do_build Dockerfile_CentOS7_Release ${VERSION}
+
+# Versioned CentOS 7 tag
+do_build Dockerfile_CentOS7_Release ${VERSION}_centos7
+
+# Versioned Ubuntu 16.04 (xenial) tag
+do_build Dockerfile_Ubuntu16.04_Release ${VERSION}_ubuntu16.04
+
