932ca8394af082cd4ee1ef343480847131cff726
Dan Nixon <dan@dan-nixon.com>
2019-04-30 09:10:22 +0100

    Proper quotes

--- mantid-development/bin/mantid_development.sh
+++ mantid-development/bin/mantid_development.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-OS=$1
-SOURCE_DIR=$2
-BUILD_DIR=$3
-DATA_DIR=$4
+OS="$1"
+SOURCE_DIR="$2"
+BUILD_DIR="$3"
+DATA_DIR="$4"
 
 docker run \
   --rm \
@@ -13,11 +13,11 @@ docker run \
   --ipc=host \
   --env PUID=`id -u` \
   --env PGID=`id -g` \
-  --env DISPLAY=$DISPLAY \
-  --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
-  --volume $HOME/.Xauthority:/home/abc/.Xauthority:ro \
-  --volume ${SOURCE_DIR}:/mantid_src \
-  --volume ${BUILD_DIR}:/mantid_build \
-  --volume ${DATA_DIR}:/mantid_data \
-  --volume mantid_development_ccache:/ccache \
-  mantidproject/mantid-development-${OS}:latest
+  --env DISPLAY="$DISPLAY" \
+  --volume "/tmp/.X11-unix:/tmp/.X11-unix:ro" \
+  --volume "$HOME/.Xauthority:/home/abc/.Xauthority:ro" \
+  --volume "$SOURCE_DIR:/mantid_src" \
+  --volume "$BUILD_DIR:/mantid_build" \
+  --volume "$DATA_DIR:/mantid_data" \
+  --volume "mantid_development_ccache_$OS:/ccache" \
+  "mantidproject/mantid-development-$OS:latest"
--- mantid-development/bin/mantid_development_x11docker.sh
+++ mantid-development/bin/mantid_development_x11docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-OS=$1
-SOURCE_DIR=$2
-BUILD_DIR=$3
-DATA_DIR=$4
+OS="$1"
+SOURCE_DIR="$2"
+BUILD_DIR="$3"
+DATA_DIR="$4"
 CMD=$5
 
 PUID=`id -u`
@@ -15,6 +15,6 @@ x11docker \
   --cap-default \
   --no-init \
   --user=RETAIN \
-  -- "--volume ${SOURCE_DIR}:/mantid_src --volume ${BUILD_DIR}:/mantid_build --volume ${DATA_DIR}:/mantid_data --env PUID=${PUID} --env PGID=${PGID}" \
-  mantidproject/mantid-development-${OS}:latest \
+  -- "--volume $SOURCE_DIR:/mantid_src --volume $BUILD_DIR:/mantid_build --volume $DATA_DIR:/mantid_data --env PUID=$PUID --env PGID=$PGID" \
+  "mantidproject/mantid-development-$OS:latest" \
   ${CMD}
