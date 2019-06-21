539d4602e329ec5d217039353b7250f86597ca94
Dan Nixon <dan@dan-nixon.com>
2019-06-21 09:20:27 +0100

    Add "can Mantid run" test
    Ensures Mantid can start correctly once installed.

--- mantid/build_helpers.sh
+++ mantid/build_helpers.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 
 IMAGE="mantidproject/mantid"
-BUILD_LOG_DIR="build_logs"
-
-mkdir -p ${BUILD_LOG_DIR}
 
 function build_image {
   DOCKERFILE=$1
-  TAG=$2
-  PACKAGE=$3
-  PATH_ADDITIONS=$4
+  EXPECTED_VERSION=$2
+  TAG=$3
+  PACKAGE=$4
+  PATH_ADDITIONS=$5
 
-  mkdir -p ${BUILD_LOG_DIR}
   docker build \
     --file=${DOCKERFILE} \
     --tag=${IMAGE}:${TAG} \
     --build-arg PACKAGE=${PACKAGE} \
     --build-arg PATH_ADDITIONS=${PATH_ADDITIONS} \
-    . | tee "${BUILD_LOG_DIR}/${TAG}.log"
+    .
+  build_result=$?
+
+  if [ $build_result -ne 0 ]; then
+    echo "Build of image for tag \"$TAG\" failed"
+    exit $build_result
+  fi
+
+  # Try to do a thing in Python for the image that has just been built
+  version_test=$(docker run --rm --volume "$PWD/version_test.py:/version_test.py" mantidproject/mantid:$TAG mantidpython /version_test.py)
+
+  echo "$version_test"
+
+  if [ -n "$EXPECTED_VERSION" ]; then
+    if [ "$EXPECTED_VERSION" == "$version_test" ]; then
+      echo "Image with tag \"$TAG\" is correct"
+    else
+      echo "Image with tag \"$TAG\" failed to build correctly"
+      exit 1
+    fi
+  else
+    echo "Ignoring expected version test, just looking for something"
+    if [ -n "$EXPECTED_VERSION" ]; then
+      echo "Have \"$version_test\" for version string, close enough"
+    else
+      echo "No version string, something is probably broken"
+      exit 1
+    fi
+  fi
 }
