5d724c2e406378aac687fa8cf35f9c59c406c218
Dan Nixon <dan@dan-nixon.com>
2019-07-09 11:15:08 +0100

    Fix nightly scripts (plus general tidy)

--- mantid/build_release.sh
+++ mantid/build_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$1
-IMAGE="mantidproject/mantid"
+IMAGE='mantidproject/mantid'
 
 # Load build tools
 . build_helpers.sh
@@ -11,20 +11,20 @@ function get_package {
 }
 
 # Download RHEL 7 package
-PACKAGE_RHEL7="./mantid_rhel_7.rpm"
+PACKAGE_RHEL7='./mantid_rhel_7.rpm'
 get_package ${PACKAGE_RHEL7} "https://github.com/mantidproject/mantid/releases/download/v${VERSION}/mantid-${VERSION}-1.el7.x86_64.rpm"
 
 # Download Ubuntu Xenial package
-PACKAGE_UBUNTU_XENIAL="./mantid_ubuntu_xenial.deb"
+PACKAGE_UBUNTU_XENIAL='./mantid_ubuntu_xenial.deb'
 get_package ${PACKAGE_UBUNTU_XENIAL} "https://github.com/mantidproject/mantid/releases/download/v${VERSION}/mantid_${VERSION}-0ubuntu1_xenial1_amd64.deb"
 
-MANTID_PATH="/opt/Mantid/bin/"
+MANTID_PATH='/opt/Mantid/bin/'
 
 # "latest" tag (default to CentOS 7)
 build_image \
-  CentOS7.Dockerfile \
+  'CentOS7.Dockerfile' \
   "$VERSION" \
-  latest \
+  'latest' \
   "$PACKAGE_RHEL7" \
   "$MANTID_PATH"
 
@@ -32,23 +32,23 @@ exit
 
 # "latest" CentOS 7 tag
 build_image \
-  CentOS7.Dockerfile \
+  'CentOS7.Dockerfile' \
   "$VERSION" \
-  latest_centos7 \
+  'latest_centos7' \
   "$PACKAGE_RHEL7" \
   "$MANTID_PATH"
 
 # "latest" Ubuntu 16.04 (xenial) tag
 build_image \
-  UbuntuXenial.Dockerfile \
+  'UbuntuXenial.Dockerfile' \
   "$VERSION" \
-  latest_ubuntu16.04 \
+  'latest_ubuntu16.04' \
   "$PACKAGE_UBUNTU_XENIAL" \
   "$MANTID_PATH"
 
 # Version only tag (default to CentOS 7)
 build_image \
-  CentOS7.Dockerfile \
+  'CentOS7.Dockerfile' \
   "$VERSION" \
   "$VERSION" \
   "$PACKAGE_RHEL7" \
@@ -56,7 +56,7 @@ build_image \
 
 # Versioned CentOS 7 tag
 build_image \
-  CentOS7.Dockerfile \
+  'CentOS7.Dockerfile' \
   "$VERSION" \
   "${VERSION}_centos7" \
   "$PACKAGE_RHEL7" \
@@ -64,7 +64,7 @@ build_image \
 
 # Versioned Ubuntu 16.04 (xenial) tag
 build_image \
-  UbuntuXenial.Dockerfile \
+  'UbuntuXenial.Dockerfile' \
   "$VERSION" \
   "${VERSION}_ubuntu16.04" \
   "$PACKAGE_UBUNTU_XENIAL" \
--- mantid/jenkins_build_nightly.sh
+++ mantid/jenkins_build_nightly.sh
@@ -2,68 +2,64 @@
 
 # ARTIFACT_DIR must be in the Docker build context (simplest solution is the
 # same directory as the Dockerfiles)
-ARTIFACT_DIR="artifacts"
-mkdir -p "${ARTIFACT_DIR}"
+ARTIFACT_DIR='artifacts'
+mkdir -p "$ARTIFACT_DIR"
 
 # Load build tools
 . build_helpers.sh
 
 # Login to Docker Hub
-docker --config=${WORKSPACE}/.docker login --username=${DOCKER_HUB_USERNAME} --password=${DOCKER_HUB_PASSWORD}
+docker --config="$WORKSPACE/.docker" login --username="$DOCKER_HUB_USERNAME" --password="$DOCKER_HUB_PASSWORD"
 
 function build_and_push {
   DOCKERFILE=$1
   TAG=$2
-  PATH_ADDITIONS=$3
-  PACKAGE_FILENAME_PATTERN=$4
+  PACKAGE_FILENAME_PATTERN=$3
+  PATH_ADDITIONS=$4
 
   # Look for a package in copied artifacts
-  PACKAGE_FILENAME=`ls ${ARTIFACT_DIR}/${PACKAGE_FILENAME_PATTERN}`
+  PACKAGE_FILENAME=`ls $ARTIFACT_DIR/$PACKAGE_FILENAME_PATTERN`
 
   # If package exists in copied artifacts
-  if [ -n "${PACKAGE_FILENAME}" ] && [ -f "${PACKAGE_FILENAME}" ]; then
-    echo "Found package \"${PACKAGE_FILENAME}\" for tag \"${TAG}\""
+  if [ -n "$PACKAGE_FILENAME" ] && [ -f "$PACKAGE_FILENAME" ]; then
+    echo "Found package \"$PACKAGE_FILENAME\" for tag \"$TAG\""
     # Build Docker image
-    build_image ${DOCKERFILE} ${TAG} "${PACKAGE_FILENAME}" "${PATH_ADDITIONS}"
+    build_image "$DOCKERFILE" '' "$TAG" "$PACKAGE_FILENAME" "$PATH_ADDITIONS"
     # Push to Docker Hub
-    docker --config=${WORKSPACE}/.docker push mantidproject/mantid:${TAG}
+    docker --config="$WORKSPACE/.docker" push "mantidproject/mantid:$TAG"
   fi
 }
 
-NIGHTLY_PYTHON2_PATH="/opt/mantidnightly/bin/"
-NIGHTLY_PYTHON3_PATH="/opt/mantidnightly-python3/bin/"
+NIGHTLY_PYTHON2_PATH='/opt/mantidnightly/bin/'
+NIGHTLY_PYTHON3_PATH='/opt/mantidnightly-python3/bin/'
 
 # Build nightly images
 build_and_push \
-  CentOS7.Dockerfile \
-  '' \
+  'CentOS7.Dockerfile' \
   'nightly' \
   "*el7*.rpm" \
-  ${NIGHTLY_PYTHON2_PATH}
+  "$NIGHTLY_PYTHON2_PATH"
 
 build_and_push \
-  CentOS7.Dockerfile \
-  '' \
+  'CentOS7.Dockerfile' \
   'nightly_centos7' \
   "*el7*.rpm" \
-  ${NIGHTLY_PYTHON2_PATH}
+  "$NIGHTLY_PYTHON2_PATH"
 
 build_and_push \
-  UbuntuXenial.Dockerfile \
-  '' \
+  'UbuntuXenial.Dockerfile' \
   'nightly_ubuntu16.04' \
   "mantidnightly_*xenial*.deb" \
-  ${NIGHTLY_PYTHON2_PATH}
+  "$NIGHTLY_PYTHON2_PATH"
 
 build_and_push \
-  UbuntuXenial.Dockerfile \
-  '' \
+  'UbuntuXenial.Dockerfile' \
   'nightly_ubuntu16.04_python3' \
   "mantidnightly-python3_*xenial*.deb" \
-  ${NIGHTLY_PYTHON3_PATH}
+  "$NIGHTLY_PYTHON3_PATH"
 
 # Logout of Docker Hub
-docker --config=${WORKSPACE}/.docker logout
+docker --config="$WORKSPACE/.docker" logout
 
 # Remove artifacts
-rm -r ${ARTIFACT_DIR}
+rm -r "$ARTIFACT_DIR"
