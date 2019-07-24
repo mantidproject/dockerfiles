f75bf13d48f0c1bf0bbdb2bbcd8d003c35ea5562
Dan Nixon <dan@dan-nixon.com>
2019-07-24 09:25:44 +0100

    Remove the cloud deply script
    AFAIK this was never really used, and the current deploy script is
    mostly stable.

--- mantid-jenkins-nodes/bin/cloud_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# A version of deploy.sh that always pulls an up to date script from the Git repo
-# No signing is done so obviously don't use this version if you care about security
-
-SCRIPT_URL="https://raw.githubusercontent.com/mantidproject/dockerfiles/master/mantid-jenkins-nodes/bin/deploy.sh"
-curl --silent --output - "$SCRIPT_URL" | bash -s -- $@
