54eca2286b32115f65b6ed713eeec2133b7ded33
Dan Nixon <dan@dan-nixon.com>
2019-08-08 08:53:56 +0100

    Allow Netdata to be semi-automatically upgraded

--- jenkins-node/bin/deploy_netdata.sh
+++ jenkins-node/bin/deploy_netdata.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
+# Ensure we have the latest version
+echo '=== Pulling latest image'
+docker pull netdata/netdata
+
+# Search for any existing container with the given name
+EXISTING_CONTAINER_ID=`docker container ls --all --format="{{.ID}}" --filter name='netdata'`
+
+# Stop and remove the container if there is one
+if [[ -n ${EXISTING_CONTAINER_ID} ]]
+then
+  echo "=== Stopping and removing existing container with ID $EXISTING_CONTAINER_ID"
+  docker stop "$EXISTING_CONTAINER_ID"
+  docker container rm "$EXISTING_CONTAINER_ID"
+fi
+
+# Start a new container
+echo '=== Starting new container'
 set -x
-
 docker run \
   --name=netdata \
   --detach \
