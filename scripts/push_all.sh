01939f1d84c4f71cee96ee487d125827a0b59101
Martyn Gigg <martyn.gigg@gmail.com>
2021-03-18 14:22:43 +0000

    Fix paths to push scripts for wrapper push

--- scripts/push_all.sh
+++ scripts/push_all.sh
@@ -5,6 +5,9 @@ set -e
 
 ROOT_DIR="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 
-$ROOT_DIR/development/docker/push.sh
-$ROOT_DIR/jenkins-node/docker-images/os-builder/push.sh
-$ROOT_DIR/jenkins-node/docker-images/static-analysis/push.sh
+cd $ROOT_DIR/development/docker/
+./push.sh
+cd $ROOT_DIR/jenkins-node/docker-images/os-builder
+./push.sh
+cd $ROOT_DIR/jenkins-node/docker-images/static-analysis
+./push.sh
