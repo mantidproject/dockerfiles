98f4b29d40c9838296ddef4543d2f606169e5a20
Silke Schomann <silke.schomann@stfc.ac.uk>
2023-06-21 16:16:04 +0100

    corrected version number

--- Linux/jenkins-node/docker/build_common_slim.sh
+++ Linux/jenkins-node/docker/build_common_slim.sh
@@ -3,10 +3,4 @@
 # Use GitHub packages container registry
 REGISTRY="ghcr.io"
 ORG="mantidproject"
-<<<<<<< HEAD
-VERSION="0.15"
-||||||| parent of c427887 (Updated version number and added fix for permission problem)
-VERSION="0.14"
-=======
 VERSION="0.16"
->>>>>>> c427887 (Updated version number and added fix for permission problem)
