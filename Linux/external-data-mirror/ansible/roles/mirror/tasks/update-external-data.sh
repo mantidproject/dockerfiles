cd3d33b8f20f63825fef4653df0be9e82abd1c45
Caila Finn <caila.finn@stfc.ac.uk>
2024-09-23 13:29:28 +0100

    Fix syntax error in script
    RE #99

--- Linux/external-data-mirror/ansible/roles/mirror/tasks/update-external-data.sh
+++ Linux/external-data-mirror/ansible/roles/mirror/tasks/update-external-data.sh
@@ -1,10 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 
 SERVER_IP=${1}
 
 RSYNC_PROCESS_IDS=$(pidof rsync)
 
-if [[ -z $RSYNC_PROCESS_IDS ]]; then
+printf "%(%H:%M:%S)T "
+
+if [ -z "${RSYNC_PROCESS_IDS}" ]; then
+        echo "running rsync..."
         rsync -az --perms -o -g  $SERVER_IP:/srv/$SERVER_IP/ftp/external-data/MD5/ /external-data/MD5/
 else
         echo "rsync is already running. Skipping this time..."
