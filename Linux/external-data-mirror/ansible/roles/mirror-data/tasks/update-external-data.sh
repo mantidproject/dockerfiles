c53c0b49786af93bd60b3e3477af26d1368605ce
Caila Finn <caila.finn@stfc.ac.uk>
2024-09-25 12:01:43 +0100

    Allow rsync with mantidproject.org hostname
    RE #99

--- Linux/external-data-mirror/ansible/roles/mirror-data/tasks/update-external-data.sh
+++ Linux/external-data-mirror/ansible/roles/mirror-data/tasks/update-external-data.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 SERVER_IP=${1}
+FTP_SRV_DIR=${2}
 
 RSYNC_PROCESS_IDS=$(pidof rsync)
 
@@ -8,7 +9,7 @@ printf "%(%H:%M:%S)T "
 
 if [ -z "${RSYNC_PROCESS_IDS}" ]; then
         echo "running rsync..."
-        rsync -az --perms -o -g  $SERVER_IP:/srv/$SERVER_IP/ftp/external-data/MD5/ /external-data/MD5/
+        rsync -az --perms -o -g  $SERVER_IP:/srv/$FTP_SRV_DIR/ftp/external-data/MD5/ /external-data/MD5/
 else
         echo "rsync is already running. Skipping this time..."
 fi
