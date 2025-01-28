9647325bb9dffce2019500a180cda6d3e7ce873e
Caila Finn <caila.finn@stfc.ac.uk>
2025-01-28 13:17:19 +0000

    Use jenkins agent instead of old script
    RE #62

--- macOS/jenkins-node/ansible/roles/agent/tasks/check-connection.sh
+++ macOS/jenkins-node/ansible/roles/agent/tasks/check-connection.sh
@@ -6,7 +6,7 @@ AGENT_SECRET=${2}
 
 echo "Check if the secret or name has changed and kill the current java process if it has. "
 
-cron_entry=$(crontab -l | grep jenkins-slave.sh)
+cron_entry=$(crontab -l | grep jenkins-agent.sh)
 cron_name_and_secret=$(echo "$cron_entry" | grep -o "$AGENT_NAME .*")
 
 if [[ "$AGENT_NAME $AGENT_SECRET" != "$cron_name_and_secret" ]]; then
@@ -16,7 +16,7 @@ fi
 
 echo "Run the agent startup script in the background. "
 
-$HOME/jenkins-slave.sh $AGENT_NAME $AGENT_SECRET &
+$HOME/jenkins-agent.sh $AGENT_NAME $AGENT_SECRET &
 
 
 echo "Wait for the script to get to its hang point. "
