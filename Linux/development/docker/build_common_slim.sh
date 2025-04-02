963d4e0a0aaf0a90fb28a151422bfce9e74d8e51
Sarah Foxley <sarah.foxley@stfc.ac.uk>
2025-04-02 08:44:19 +0100

    Delete files moved to other folders

--- Linux/development/docker/build_common_slim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Use GitHub packages container registry
-REGISTRY="ghcr.io"
-ORG="mantidproject"
-VERSION="0.16"
-
-BUILD_LOG_DIR="build_logs"
--- Linux/jenkins-node/docker/build_common_slim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Use GitHub packages container registry
-REGISTRY="ghcr.io"
-ORG="mantidproject"
-VERSION="0.16"
