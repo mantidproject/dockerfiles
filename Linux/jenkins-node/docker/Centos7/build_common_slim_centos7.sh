36898d4ca017ef3ff85729175a81a60807c057aa
Sarah Foxley <sarah.foxley@stfc.ac.uk>
2025-04-02 08:42:19 +0100

    Archive old CentOS7 jenkins-node files

---
+++ Linux/jenkins-node/docker/Centos7/build_common_slim_centos7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Use GitHub packages container registry
+REGISTRY="ghcr.io"
+ORG="mantidproject"
+VERSION="0.17"
---
+++ Linux/jenkins-node/docker/Centos7/build_slim_centos7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. build_common_slim.sh
+
+docker build \
+  --file=CentOS7Slim.Dockerfile \
+  --tag=${REGISTRY}/${ORG}/jenkins-node-centos7-slim:${VERSION} \
+  .
+
+docker build \
+  --file=CentOS7Slim.Dockerfile \
+  --tag=${REGISTRY}/${ORG}/jenkins-node-centos7-slim:latest \
+  .
