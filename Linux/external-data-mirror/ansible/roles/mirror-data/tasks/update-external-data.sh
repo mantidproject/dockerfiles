ed112be25b37b50f2b5add2e65d5ba4ed88bb7be
Caila Finn <caila.finn@stfc.ac.uk>
2025-10-13 18:14:21 +0100

    Use mounts rather than volumes for ext data

--- Linux/external-data-mirror/ansible/roles/mirror-data/tasks/update-external-data.sh
+++ Linux/external-data-mirror/ansible/roles/mirror-data/tasks/update-external-data.sh
@@ -9,7 +9,7 @@ printf "%(%H:%M:%S)T "
 
 if [ -z "${RSYNC_PROCESS_IDS}" ]; then
         echo "running rsync..."
-        rsync -az --perms -o -g  $SERVER_IP:/srv/$FTP_SRV_DIR/ftp/external-data/MD5/ /external-data/MD5/
+        rsync -azvW --perms -o -g  $SERVER_IP:/srv/$FTP_SRV_DIR/ftp/external-data/MD5/ /external-data/MD5/
 else
         echo "rsync is already running. Skipping this time..."
 fi
--- Linux/jenkins-node/ansible/roles/mirror-data/tasks/update-external-data.sh
+++ Linux/jenkins-node/ansible/roles/mirror-data/tasks/update-external-data.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 SERVER_IP=${1}
+HOST_NAME=${2}
+USER_NAME=${3}
 
 RSYNC_PROCESS_IDS=$(pidof rsync)
 
@@ -8,7 +10,7 @@ printf "%(%H:%M:%S)T "
 
 if [ -z "${RSYNC_PROCESS_IDS}" ]; then
         echo "running rsync..."
-        rsync -az --perms -o -g  mkq48465@$SERVER_IP:/external-data/MD5/ /mantid_data/MD5/
+        rsync -azvW --perms -o -g  $USER_NAME@$SERVER_IP:/external-data/MD5/ /${HOST_NAME}_external_data/MD5/
 else
         echo "rsync is already running. Skipping this time..."
 fi
