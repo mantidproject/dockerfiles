2de94db78b63b74d27286c428043cd4d2d9c85df
Ian Gibbs <90706300+idigs@users.noreply.github.com>
2025-12-05 09:56:26 -0500

    update build.sh for alma9 dockerfile

--- Linux/github-runner/docker/build.sh
+++ Linux/github-runner/docker/build.sh
@@ -3,6 +3,6 @@
 . build_common.sh
 
 docker build \
-  --file=RockyLinux8.Dockerfile \
-  --tag=${REGISTRY}/${ORG}/github-runner-rockylinux8:${VERSION} \
+  --file=Alma9.Dockerfile \
+  --tag=${REGISTRY}/${ORG}/github-runner-alma9:${VERSION} \
   .
